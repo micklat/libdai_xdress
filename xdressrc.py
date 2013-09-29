95e0959324317c7b8bd94b7fad1bdad4374c761f
Micky Latowicki <biosap@gmail.com>
2013-09-29 12:32:52 +0300

    * force the use of gccxml as a parser * add inactive code for using 'autoall' on all .h files

--- xdressrc.py
+++ xdressrc.py
@@ -1,12 +1,23 @@
+import os.path
+from glob import glob
+
 package = 'dai'     # top-level python package name
 packagedir = 'dai'  # location of the python package
 sourcedir = 'src'      # location of C/C++ source
 verbose = True
 includes = ['src']
+parsers = ['gccxml']
 
 plugins = ('xdress.autoall', 'xdress.autodescribe', 'xdress.cythongen', 'xdress.stlwrap')
 extra_types = 'dai_extra_types'
 
-classes = [
-  ('VarSet', 'varset', 'dai'),
-  ]
+if True:
+  classes = []
+  #classes += [(type_name, 'util', 'dai') for type_name in ('Real', 'BigInt')]
+  classes += [('VarSet', 'varset', 'dai')]
+
+else:
+  all_stems = [os.path.basename(file_path)[:-2]
+               for file_path in glob(os.path.join(sourcedir, '*.h'))]
+  classes = [('*', stem, 'dai') for stem in all_stems]
+
